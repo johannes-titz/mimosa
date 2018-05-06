8c7fd5d684359e623591d4e119b7709a639c67f4
Maria Reichert <maria.reichert@mailbox.tu-dresden.de>
2018-05-06 21:26:45 +0200

    initial commit

---
+++ server.R
@@ -0,0 +1,121 @@
+shinyServer(function(input, output, session) {
+  
+  # initialize dataframes which will contain the variables
+  # maybe they don't really need to be reactive
+  level1 <- level2 <- data.frame()
+  vars <- reactiveValues(level1 = level1, level2 = level2)
+  
+  observeEvent(input$datafile, {
+   
+    req(input$datafile)
+    data <- read.table(file = input$datafile$datapath)
+    # test file: "C:/HLM7 Student Examples/AppendxA/HSBALL.dat"
+    
+    ## identify levels (heuristically)
+    
+    # find transition points between groups
+    # assuming that id is the first variable in the df
+    row_counter <- c(2:dim(data)[1])
+    row_counter <- row_counter[data[row_counter, 1] != data[row_counter-1, 1]]
+    
+    # check if all values until first transition point are equal
+    equal <- apply(data[1:(row_counter[1]-1), ], 2, duplicated)
+    equal <- apply(equal[2:dim(equal)[1], ], 2, all)
+    
+    # sort variables by levels
+    # assuming a maximum of 2 levels
+    vars$level1 <- data.frame(data[ , !equal])
+    vars$level2 <- data.frame(data[ , equal])
+    
+  })
+  
+  # variable inputs are generated in the server file since they depend on vars
+  output$variables <- renderUI({
+    fluidRow(
+      column(width = 3,
+             radioButtons("outcome", label = "Outcome", selected = character(0),
+                          choices = colnames(vars$level1))
+      ),
+      column(width = 3,
+             checkboxGroupInput("l1", label = "Level 1", 
+                                choiceNames = colnames(vars$level1),
+                                choiceValues = colnames(vars$level1))
+      ),
+      column(width = 3,
+             checkboxGroupInput("l2", label = "Level 2", 
+                                choiceNames = colnames(vars$level2),
+                                choiceValues = colnames(vars$level2))
+      )
+    )
+  })
+  
+  # prevent selecting outcome as predictor by removing it from choices
+  observeEvent(input$outcome, {
+    updateCheckboxGroupInput(session, "l1", 
+                             choiceNames = colnames(vars$level1)[colnames(vars$level1) != input$outcome],
+                             choiceValues = colnames(vars$level1)[colnames(vars$level1) != input$outcome],
+                             selected = input$l1)
+  })
+  
+  # create HTML output for level 1 equation
+  output$mod_l1 <- renderUI({
+    if (!is.null(input$outcome)){
+      equation <- c(input$outcome, "<sub>ij</sub> = &beta;<sub>0j</sub> ",
+                  " + e<sub>ij</sub>")
+      if (!is.null(input$l1)){
+        for (index in 1:length(input$l1)){
+          equation <- append(equation, 
+                             c(" + &beta;<sub>", index, "j</sub>", input$l1[index]), 
+                             after = length(equation)-1)
+        }
+      }
+      HTML(paste(equation, collapse = ""))
+    }
+    else if (!is.null(input$l1)){
+      HTML(paste("<p style=\"color:red\">Please select an outcome variable first.</p>"))
+    }
+  })
+  
+  # create HTML output for level 2 equations
+  # does not yet allow to modify equations independently 
+  output$mod_l2 <- renderUI({
+    if (!is.null(input$outcome)){
+      equation <- c()
+      if (1){
+        for (l1_var in 0:length(input$l1)){
+          if (l1_var > 0){
+            eq_beta <- c("<br>&beta;<sub>", l1_var, "j</sub> = &gamma;<sub>", l1_var, 
+                         "0</sub> ", " + u<sub>0j</sub>")
+          }
+          else {
+            eq_beta <- c("&beta;<sub>", l1_var, "j</sub> = &gamma;<sub>", l1_var, 
+                         "0</sub> ", " + u<sub>0j</sub>")
+          }
+            
+          if (!is.null(input$l2)){
+            for (l2_var in 1:length(input$l2)){
+              eq_beta <- append(eq_beta,
+                                c(" + &gamma;<sub>", l1_var, l2_var, "</sub>",
+                                  input$l2[l2_var]),
+                                after = length(eq_beta)-1)
+            }
+            equation <- append(equation, eq_beta)
+          }
+          else {
+            equation <- append(equation, eq_beta)
+          }
+        }
+      }
+      HTML(paste(equation, collapse = ""))
+    }
+    else if (!is.null(input$l2)){
+      HTML(paste("<p style=\"color:red\">Please select an outcome variable first.</p>"))
+    }
+      
+  })
+
+  
+
+
+
+})
---
+++ ui.R
@@ -0,0 +1,53 @@
+library(shiny)
+library(shinydashboard)
+
+shinyUI(
+
+  dashboardPage(skin = "blue",
+              
+    dashboardHeader(title = "Mixed Models"),
+    
+    dashboardSidebar(
+    
+      tags$head(tags$style(HTML('.sidebar {padding-left: 8px;}'))),
+      
+      h4("Load data"),
+      fileInput("datafile", label = NULL),
+      helpText("Some options to correctly load data (e.g. variable names in first line, file type, separator, ...)")
+    ),
+  
+    dashboardBody(
+
+      fluidRow(
+
+        # in this box variables are shown (sorted by levels) and can be chosen for the model
+        box(title = "Variables", status = "primary", collapsible = T,
+            
+            uiOutput("variables")
+        ),
+        
+        # in this box the model equations will be displayed
+        box(title = "Model", status = "primary", collapsible = T,
+            
+            strong("Level 1"), br(),
+            uiOutput("mod_l1"),
+            br(), strong("Level 2"),
+            helpText("# lacks the possibility of choosing different predictors in each equation"),
+            uiOutput("mod_l2")
+        )
+      ),
+      
+      fluidRow(
+        
+        # in this box the results will be shown
+        box(title = "Output Table", status = "primary", width = 12,
+            
+            helpText("Table showing results"),
+            helpText("-> table output?, download button?")
+        )
+        
+      )
+    
+    )
+  )
+)
