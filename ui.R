756f1cd0b237dc4a784921b50a4c44163897f8c6
Johannes Titz <johannes.titz@gmail.com>
2019-03-13 11:29:01 +0100

    add libs

--- ui.R
+++ ui.R
@@ -4,40 +4,33 @@ library(lme4)
 library(xtable)
 library(sjPlot)
 library(readr)
-source("helper.R")
 library(shinyalert)
+library(Hmisc)
+library(dplyr)
+source("helper.R")
 
-options(shiny.autoreload = T)
+options(shiny.autoreload = F)
 
 shinyUI(
-
-
   dashboardPage(skin = "blue",
-                
     dashboardHeader(title = "mimosa"),
-    
     dashboardSidebar(
       useShinyalert(),
       tags$head(tags$style(HTML('.sidebar {padding-left: 8px;}'))),
-      
       h4("Load data"),
       fileInput("datafile", label = NULL),
       helpText("Some options to correctly load data (e.g. variable names in first line, file type, separator, ...)")
     ),
   
     dashboardBody(
-
       fluidRow(
-
         # in this box variables are shown (sorted by levels) and can be chosen for the model
         box(title = "Variables", status = "primary", collapsible = T, width = 7,
-            
             uiOutput("variables")
         ),
         
         # in this box the model equations will be displayed
         box(title = "Model", status = "primary", collapsible = T, width = 5,
-            
             strong("Level 1"), br(),
             uiOutput("mod_l1"),
             br(), strong("Level 2"),
@@ -45,16 +38,12 @@ shinyUI(
             uiOutput("mod_l2")
         )
       ),
-      
       fluidRow(
-        
         # in this box the results will be shown
         box(title = "Output Table", status = "primary", width = 12,
         uiOutput("table_region")
         )
-        
       )
-    
     )
   )
 )
