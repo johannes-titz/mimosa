7f75530d4ea707f114a7350d77a3beede611f570
Johannes <johannes@titz.science>
2019-12-02 16:24:39 +0100

    add simple test for math formula

--- tests/testthat/test-app-file.R
+++ tests/testthat/test-app-file.R
@@ -10,5 +10,6 @@ test_that("mimosa works", {
   # Use compareImages=FALSE because the expected image screenshots were created
   # on a Mac, and they will differ from screenshots taken on the CI platform,
   # which runs on Linux.
-   expect_pass(testApp("app/", c("mytest", "table_options"), compareImages = FALSE))
+   expect_pass(testApp("app/", c("mytest", "table_options", "formula"),
+                       compareImages = FALSE))
 })
