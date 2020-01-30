a4980ed935e0a0722402d3eff757db4b32e5c526
Johannes <johannes@titz.science>
2020-01-30 13:38:31 +0100

    add tests for detecting group variable

--- tests/testthat/test-app-file.R
+++ tests/testthat/test-app-file.R
@@ -13,3 +13,19 @@ test_that("mimosa works", {
    expect_pass(testApp("app/", c("mytest", "table_options"),
                        compareImages = FALSE))
 })
+
+test_that("finding grouping variable works", {
+  # first variable is always the correct id
+  expect_identical("schoolNR", find_id(mlmRev::bdf))
+  expect_identical(c("school", "schgend"), find_id(mlmRev::Exam))
+  expect_identical(find_id(mlmRev::Exam), find_id(mlmRev::Exam[10:1]))
+  expect_identical("Subject", find_id(lme4::sleepstudy))
+  expect_identical(c("district", "livch", "use", "urban"), find_id(mlmRev::Contraception))
+  expect_identical(find_id(mlmRev::Contraception), find_id(mlmRev::Contraception[,6:1]))
+  expect_identical("id", find_id(mlmRev::Early))
+  expect_identical(c("school", "gender"), find_id(mlmRev::Gcsemv))
+  expect_identical(find_id(mlmRev::Gcsemv), find_id(mlmRev::Gcsemv[5:1]))
+  expect_identical("school", find_id(mlmRev::Hsb82))
+  expect_identical(c("Subject", "Occasion"), find_id(mlmRev::Oxboys))
+  expect_identical(find_id(mlmRev::Oxboys), find_id(mlmRev::Oxboys[4:1]))
+})
