53775a0e491230f8d3b2afbd61924eb0535e3707
Johannes <johannes@titz.science>
2020-01-31 10:44:05 +0100

    split up tests

--- tests/testthat/test-app-file.R
+++ tests/testthat/test-app-file.R
@@ -3,15 +3,45 @@ context("app-function")
 
 library(shinytest)
 
-test_that("mimosa works", {
+test_that("checkbox selection works", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
 
   # Use compareImages=FALSE because the expected image screenshots were created
   # on a Mac, and they will differ from screenshots taken on the CI platform,
   # which runs on Linux.
-   expect_pass(testApp("app/", c("mytest", "table_options", "formula",
-                                 "mlmRev"),
+   expect_pass(testApp("app/", c("mytest"), compareImages = FALSE))
+})
+
+test_that("table options work", {
+  # Don't run these tests on the CRAN build servers
+  skip_on_cran()
+
+  # Use compareImages=FALSE because the expected image screenshots were created
+  # on a Mac, and they will differ from screenshots taken on the CI platform,
+  # which runs on Linux.
+   expect_pass(testApp("app/", c("table_options"), compareImages = FALSE))
+})
+
+test_that("formula display works", {
+  # Don't run these tests on the CRAN build servers
+  skip_on_cran()
+
+  # Use compareImages=FALSE because the expected image screenshots were created
+  # on a Mac, and they will differ from screenshots taken on the CI platform,
+  # which runs on Linux.
+   expect_pass(testApp("app/", c("formula"),
+                       compareImages = FALSE))
+})
+
+test_that("estimates are correct for Exam data", {
+  # Don't run these tests on the CRAN build servers
+  skip_on_cran()
+
+  # Use compareImages=FALSE because the expected image screenshots were created
+  # on a Mac, and they will differ from screenshots taken on the CI platform,
+  # which runs on Linux.
+   expect_pass(testApp("app/", c("mlmRev"),
                        compareImages = FALSE))
 })
 
