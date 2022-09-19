c2e3c3fec36a9ad56796acf58dccddcd05291c99
Johannes <johannes@titz.science>
2022-09-19 15:25:16 +0200

    add port and host to run_app

--- R/run_app.R
+++ R/run_app.R
@@ -1,10 +1,13 @@
 #' Starts the mimosa shiny app
-#'
+#' @param host default is from getOption shiny.host 
+#' @param port default is from getOption shiny.port
 #' @import shiny
 #' @export
-run_app <- function() {
+run_app <- function(host = getOption("shiny.host"),
+                    port = getOption("shiny.port")) {
   shinyApp(
     ui = myui(),
-    server = myserver
+    server = myserver,
+    options = list(host = host, port = port)
   )
 }
