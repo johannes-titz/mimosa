f2d4e1d6e3fbebc6b2dde89169f4b6b7e97b0bbb
Johannes <johannes@titz.science>
2023-09-13 13:33:34 +0200

    style: improve create_table

--- R/output.R
+++ R/output.R
@@ -7,7 +7,7 @@
 #' @param l1 level 1 variables
 #' @param output_options what to show in the output
 #' @noRd
-create_table <- function(mdl, l1, output_options){
+create_table <- function(mdl, l1, output_options) {
   check <- c("standard error", "AIC", "Deviance", "Log-Likelihood",
              "standardized coefficients", "test statistic", "p-value") 
   
@@ -19,18 +19,20 @@ create_table <- function(mdl, l1, output_options){
   } else {
     show_beta <- NULL
   }
-  sjPlot::tab_model(mdl,
-                    show.se = show["standard error"], 
-                    show.p = show["p-value"],
-                    show.stat = show["test statistic"],
-                    show.aic = show["AIC"],
-                    show.dev = show["Deviance"],
-                    show.loglik = show["Log-Likelihood"],
-                    show.std = show_beta,
-                    string.se = "SE",
-                    string.std = "&beta;",
-                    string.ci = "95% CI",
-                    string.stat = "<i>t</i>",
-                    collapse.ci = F, show.icc = TRUE, show.re.var = TRUE,
-                    show.ngroups = TRUE, show.fstat = FALSE, show.aicc = F)[[3]]
+  sjPlot::tab_model(
+    mdl,
+    show.se = show["standard error"], 
+    show.p = show["p-value"],
+    show.stat = show["test statistic"],
+    show.aic = show["AIC"],
+    show.dev = show["Deviance"],
+    show.loglik = show["Log-Likelihood"],
+    show.std = show_beta,
+    string.se = "SE",
+    string.std = "&beta;",
+    string.ci = "95% CI",
+    string.stat = "<i>t</i>",
+    collapse.ci = F, show.icc = TRUE, show.re.var = TRUE,
+    show.ngroups = TRUE, show.fstat = FALSE, show.aicc = F
+  )[[3]]
 }
