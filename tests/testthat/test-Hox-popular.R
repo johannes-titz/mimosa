4a7a0c44f1df8780c8637e337216f0d1f04108eb
Johannes <johannes@titz.science>
2023-09-15 15:32:30 +0200

    tests: add last example of Hox popularity2

--- tests/testthat/test-Hox-popular.R
+++ tests/testthat/test-Hox-popular.R
@@ -19,4 +19,27 @@ test_that("Hox Example Popularity is reproducible", {
   expect_true(grepl('2">0.70', outputtable))
   # ICC
   expect_true(grepl('2">0.36', outputtable))
+  
+  # last model, with level 2 effect and cross-level-interaction
+  # note that sex is not a random effect
+  app$set_inputs(l1 = c("extrav", "sex"),  l2 = c("texp"))
+  app$set_inputs(l1_varies = c("extrav"))
+  app$set_inputs(interaction = c("extrav:texp"))
+
+  outputtable <- app$get_value(output = "table_region")$html
+  # always in column 2
+  # a teacher with 0 years of experience has an expected popularity of -1.207
+  # intercept &#45; = -
+  expect_true(grepl('2">&#45;1.21', outputtable))
+  # extraversion effect
+  expect_true(grepl('2">0.80', outputtable))
+  # teacherxperience effect
+  expect_true(grepl('2">0.23', outputtable))
+  # moderator of teacherxp on relationship between extraversion and popularity
+  expect_true(grepl('2">&#45;0.02', outputtable))
+  
+  # sigma^2
+  expect_true(grepl('2">0.55', outputtable))
+  # tau seems to differ slightly
+  # ICC not clear
 })
